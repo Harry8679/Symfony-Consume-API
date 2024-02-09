aad344dde7ad68961aec9c9a215a61c559d262a5
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-09 12:09:00 +0100

    decode method to convert json format to array

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,16 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ApiController extends AbstractController
 {
     #[Route('/liste-regions', name: 'listRegion')]
-    public function index(): Response
+    public function index(SerializerInterface $serializer): Response
     {
         $mesRegions = file_get_contents('https://geo.api.gouv.fr/regions');
+        $mesRegionsArray = $serializer->decode($mesRegions, 'json');
 
-        dd($mesRegions);
+        // dd($mesRegionsArray);
         
-        return $this->render('api/index.html.twig');
+        return $this->render('api/index.html.twig', [
+            'mesRegionsArray' => $mesRegionsArray
+        ]);
     }
 }
