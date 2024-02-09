680fb7f280e8586fac3d0af080199ab373a2aba5
Harry EMARH <emarh.harry.code@gmail.com>
2024-02-09 12:24:19 +0100

    Region class created

---
+++ src/Entity/Region.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+class Region
+{
+    private ?int $id = null;
+
+    private ?string $code = null;
+
+    private ?string $nom = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+}
